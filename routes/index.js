41d834a8bbcba67525f920a884fce8a7253f6298
emicarnicero <ecarnicero@qplus.com.ar>
2019-11-06 14:26:11 +0100

    Change welcome message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World Express Jenkins' });
+  res.render('index', { title: 'Hello World Express' });
 });
 
 module.exports = router;
